b52f03a727d022bf543d7a837955099ecfcaf0a7
Anuj Visavale <flamingqaz@gmail.com>
2019-01-26 13:24:50 -0500

    Added Sensor class and turretLeftLimitSwitch & turretRightLimitSwitch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,8 +16,11 @@ public static final int CHASSIS_RIGHT_MASTER = 8;
 public static final int CHASSIS_RIGHT_SLAVE = 9;
 
 public static final double MAX_MOTOR_SPEED = 0.9;
+public static final double TURRET_STOP = 0.0;
 
 public static final double SHOOTER_STOP = 0.0;
 
+public static final int TURRET_LEFT_LIMIT_SWITCH = 4;
+public static final int TURRET_RIGHT_LIMIT_SWITCH = 5;
 
 }
--- src/main/java/frc/robot/Turret.java
+++ src/main/java/frc/robot/Turret.java
@@ -3,6 +3,12 @@ package frc.robot;
 public class Turret {
     public static void turret(double turretSpeed) {
         double turnSpeed = Math.min(turretSpeed, Constants.MAX_MOTOR_SPEED);
-        Actuators.getTurret().set(turnSpeed);
+        if ((turnSpeed < 0 && Sensors.getTurretLeftLimitSwitch().get()) || 
+                (turnSpeed > 0 && Sensors.getTurretRightLimitSwitch().get())) {
+            
+             Actuators.getTurret().set(Constants.TURRET_STOP);
+        }else {
+            Actuators.getTurret().set(turnSpeed);
+        }
     }
 }
